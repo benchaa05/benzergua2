4240cc0862dd1a5ffc222f920dd6fdc6b2e5577a
benchaa05 <benchaa05@gmail.com>
2015-01-18 23:14:56 +0100

    12

--- benzergua/MainWindow.xaml.cs
+++ benzergua/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,17 +37,66 @@ namespace benzergua
         {
             gmdbEntities gmdb = new gmdbEntities();
             var query = from t in gmdb.patients
+                where t.PatientID >= 30000
+                // && t.PatientID < 30000
                 select t;
 
             foreach (var getPatient in query)
             {
-                _patientManager.CreatePatientUniqueCode(getPatient);
+                string getCode = GetCode(getPatient);
+                getPatient.PatUniqueCode = getCode;
+
+                // _patientManager.CreatePatientUniqueCode(getPatient);
             }
+            gmdb.SaveChanges();
             MessageBox.Show("finished");
 
             var query2 = from t in gmdb.patients
                 select t;
             PatientGridControl.ItemsSource = query2.ToList();
+        }
+
+        private string GetCode(patient getPatient)
+        {
+            try
+            {
+                string getedCode;
+                int rightnumber = DateTime.Now.Year;
+                string numbers = rightnumber.ToString(CultureInfo.InvariantCulture);
+                string[] righttwodigits = numbers.Split('0');
+                string twodigits = righttwodigits[1];
+                string valueOfPatientID = getPatient.PatientID.ToString(CultureInfo.InvariantCulture);
+                switch (valueOfPatientID.Length)
+                {
+                    case 1:
+                        const string tenthousand = "0000";
+                        getedCode = string.Concat(tenthousand, valueOfPatientID, "/", twodigits);
+                        break;
+                    case 2:
+                        const string thousand = "000";
+                        getedCode = string.Concat(thousand, valueOfPatientID, "/", twodigits);
+                        break;
+                    case 3:
+                        const string hundred = "00";
+                        getedCode = string.Concat(hundred, valueOfPatientID, "/", twodigits);
+                        break;
+                    case 4:
+                        const string ten = "0";
+                        getedCode = string.Concat(ten, valueOfPatientID, "/", twodigits);
+                        break;
+                    case 5:
+                        getedCode = string.Concat(valueOfPatientID, "/", twodigits);
+                        break;
+                    default:
+                        getedCode = "";
+                        break;
+                }
+                return getedCode;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
         }
     }
 }
